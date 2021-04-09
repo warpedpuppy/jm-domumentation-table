40d1ae41cf593621c57f644a108f3396db213700
cuna0 <77469728+cuna0@users.noreply.github.com>
2021-04-09 08:18:46 -0700

    added CRUD

--- index.js
+++ index.js
@@ -5,43 +5,53 @@ morgan = require('morgan');
 let Movies = [
   {
     title: 'BLACK PANTHER',
-   
+    genre:  'Action',
+    director: 'Ryan Coogler',
   },
   {
     title: 'Citizen Kane',
-   
+    genre:  'Mystery',
+    director: 'orson Welles',
   },
   {
     title: 'Parasite',
-    
+    genre:  'Comedy',
+    director: 'Bong Joon-ho',
   }, 
   {
     title: 'Casablanca',
-    
+    genre:  'Romance',
+    director: 'Michael Curtiz',
   },
   {
     title: 'Avengers: Endgame',
-    
+    genre:  'Action',
+    director: 'Anthony Russo',
   },
   {
     title: 'Knives Out',
-    
+    genre:  'Mystery',
+    director: 'Rian Johnson',
   },
   {
     title: 'Us',
-    
+    genre:  'Horror', 
+    director: 'Jordan Peele',
   },
   {
     title: 'Toy Story',
-    
+    genre:  'Animation',
+    director: 'John lasseter',
   },
   {
     title: 'Lady Bird',
-    
+    genre:  'Comedy',
+    director: 'Greta Gerwig',
   },
   {
-    title: 'Mission: Impossible - Fallout'
-    
+    title: 'Mission: Impossible - Fallout',
+    genre:  'Action',
+    director: 'Christopher McQuarrie',
   }
 ];
 
@@ -63,9 +73,46 @@ app.get('/movies', (req, res) => {
   res.json(Movies);
 });
 
+app.get('/movies/titles', (req, res) => {
+  res.send('movies and titles');
+}); 
+
+app.get('/movies/genre', (req, res) => {
+  res.send('movies by genre');
+}); 
+
+app.get('/movies/director', (req, res) => {
+  res.send('movies by directors');
+}); 
+
+// Post requests
+
+app.post('/user', (req, res) => {
+  res.send('New user');
+}); 
+
+app.post('/user/name/add', (req, res) => {
+  res.send('movie successfully added');
+}); 
+
+// Put requests 
+
+app.put('/user/name/edit', (req, res) => {
+  res.send('user successfully edited');
+}); 
+
+
+// Delete requests 
+
+app.delete('/user/name/info', (req, res) => {
+  res.send('Successfully deleted user');
+});  
+
+app.delete('/user/name/delete', (req, res) => {
+  res.send('Successfully deleted movie');
+});  
+
 // Logging
-
-
 
 app.use(morgan('common'));
 
